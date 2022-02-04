152b24bee26b2f2a7156015b5d7cf74cbf0576a8
Przemysław Wnęk <wnek.przemek@gmail.com>
2022-02-04 18:48:43 +0100

    use one state as obj with state spread

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,23 +3,38 @@ import React, { useState } from "react";
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-  const [enteredTitle, setEnteredTitle] = useState('');
-  const [enteredAmount, setEnteredAmount] = useState('');
-  const [enteredDate, setEnteredDate] = useState('');
+//   const [enteredTitle, setEnteredTitle] = useState('');
+//   const [enteredAmount, setEnteredAmount] = useState('');
+//   const [enteredDate, setEnteredDate] = useState('');
+
+const [userInput, setUserInput ] = useState({
+    enteredTitle: '',
+    enteredAmount: '',
+    enteredDate: '',
+
+})
 
 
   const titleChangehandelr = (event) => {
-    setEnteredTitle(event.target.value);
+    setUserInput({
+        ...userInput,
+        enteredTitle: event.target.value,
+    });
   }
 
   const amountChangehandelr = (event) => {
-    setEnteredAmount(event.target.value);
+    setUserInput({
+        ...userInput,
+        enteredAmount: event.target.value,
+    });
   }
 
   const dateChangehandelr = (event) => {
-    setEnteredDate(event.target.value);
+    setUserInput({
+        ...userInput,
+        enteredDate: event.target.value,
+    });
   }
-
 
   return (
     <form>
